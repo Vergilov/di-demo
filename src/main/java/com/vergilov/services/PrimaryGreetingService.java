9f42fdc4fa24ed1928223377e7c647f7809570ce
Vergilov <biggohunter@gmail.com>
2019-10-06 21:12:10 +0200

    Add Spanish

---
+++ src/main/java/com/vergilov/services/PrimaryGreetingService.java
@@ -0,0 +1,15 @@
+package com.vergilov.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("en")
+public class PrimaryGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello from the Primary Greeting service";
+    }
+}
